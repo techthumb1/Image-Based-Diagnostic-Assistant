14360e64493879c4c477e52b8ed66cf34e7e5575
Jason Robinson <robinsonjason761@gmail.com>
2024-06-03 15:27:14 -0500

    General commit

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,17 @@
 import unittest
-from model.cnn_model import model_predict
+from models.model import create_model
 
-class TestModelPredict(unittest.TestCase):
-    def test_prediction_output(self):
-        # Assume you have a sample image path and a function to handle prediction
-        result = model_predict('path_to_sample_image.jpg')
-        self.assertIsNotNone(result)
+class ModelTestCase(unittest.TestCase):
+    def test_model_load(self):
+        """Test model loading."""
+        self.assertIsInstance(create_model(), tf.keras.models.Model)
+
+    def test_prediction(self):
+        """Test model prediction."""
+        model = create_model()
+        test_input = tf.random.normal([1, 224, 224, 3])
+        prediction = model.predict(test_input)
+        self.assertEqual(prediction.shape, (1, 1))
 
 if __name__ == '__main__':
     unittest.main()
