72f766aa06feace3a34a1ec29e617ab2366f85e6
Jason Robinson <robinsonjason761@gmail.com>
2024-06-04 21:08:06 -0500

    Update src/App.js in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import UploadPage from './components/UploadPage';
 import ResultPage from './components/ResultPage';
@@ -7,12 +7,12 @@ import AboutPage from './components/AboutPage';
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/upload" component={UploadPage} />
-      <Route path="/results" component={ResultPage} />
-      <Route path="/about" component={AboutPage} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/upload" element={<UploadPage />} />
+      <Route path="/results" element={<ResultPage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
   </Router>
 );
 
